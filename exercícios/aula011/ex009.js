172d1ae031d83e31b58b506d266dcc37dcfde85c
Daniel Ribeiro Silva <dand.danielribeiro@gmail.com>
2026-02-07 16:05:33 -0300

    Initial commit

---
+++ exercícios/aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`vivendo em ${país}`)
+if (país == 'Brasil') {//condição composta
+    console.log('brasileiro')
+}else {
+    console.log('estrangeiro')
+} 
---
+++ exercícios/aula012/ex012.js
@@ -0,0 +1,12 @@
+let agora = new Date()
+let hora = agora.getHours();
+console.log(`agora são ${hora} horas.`)
+if (hora < 12){
+    console.log("bom dia!")
+}
+else if (hora <= 18) {
+        console.log("boa tarde!")
+}
+else {
+    console.log("boa noite!")
+}
